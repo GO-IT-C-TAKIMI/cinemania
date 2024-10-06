b39e96cd0ec858aa4c71eb24305e16175d5b307a
bahacan112 <bzenbil19@gmail.com>
2024-10-06 19:13:01 +0300

    git ayarları eklendi

--- src/js/fetchingdata.js
+++ src/js/fetchingdata.js
@@ -1,10 +1,25 @@
-const API_KEY = import.meta.env.TMDB_API_KEY;
-const BASE_URL = import.meta.env.TMDB_API_URL;
-
 export async function getMovies() {
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const BASE_URL = import.meta.env.VITE_TMDB_API_URL;
   const response = await fetch(
     `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
   );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
   const data = await response.json();
-  return data.results;
+  return JSON.stringify(data);
 }
+
+async function fetchMovies() {
+  try {
+    const movies = await getMovies();
+    console.log(movies);
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+}
+
+fetchMovies();
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { getMovies } from './js/fetchingdata';
+
+getMovies();
