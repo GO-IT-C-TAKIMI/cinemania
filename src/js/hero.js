2e7cc82e4fe13059e0530385453c000eb29978dd
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-10-09 22:00:59 +0300

    hero changes

--- src/js/hero.js
+++ src/js/hero.js
@@ -6,7 +6,6 @@ export function hero() {
     'movies-description-container'
   );
 
-
   const fetchTrendingByDay = async () => {
     try {
       const res = await fetch(
@@ -28,39 +27,47 @@ export function hero() {
   };
 
   const displayMovie = async movie => {
+    const rating = movie.vote_average;
+    
     try {
       const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${movie.id}/images?&api_key=${api_key}`
+        `https://api.themoviedb.org/3/movie/${movie.id}/images?&api_key=${api_key}&language=en&language=null`
       );
       const imageData = await res.json();
-      const movieImage = imageData.backdrops[0];
+      const movieImage =
+        imageData.backdrops[
+          Math.floor(Math.random() * imageData.backdrops.length)
+        ];
       const movieOverview = movie.overview.split(' ').slice(0, 21).join(' ');
       imageContainer.innerHTML += `<img class="image" src="https://image.tmdb.org/t/p/original${movieImage.file_path}" />`;
       descriptionContainer.innerHTML += `<h1 class="movie-title">${movie.title}</h1>
-    <ul class="stars">
-    <li>x</li>
-    <li>x</li>
-    <li>x</li>
-    <li>x</li>
-    <li>x</li>
-    </ul>
     <p class="movie-desc">${movieOverview} ...</p>
     <div class="hero-movie-buttons">
     <button id="trailer" class="orange-button btn-hero trailer">Watch trailer</button>
     <button id="details" class="white-button btn-hero details">More details</button>
-</div>`;
+      </div>`;
 
       const trailerButton = document.getElementById('trailer');
       const detailsButton = document.getElementById('details');
       detailsButton.addEventListener('click', () => {
         console.log('detay butonuna tiklandi');
-      })
-      trailerButton.addEventListener('click', () => {
-        console.log('trailer butonuna tiklandi');
       });
-
-      
-
+      trailerButton.addEventListener('click', async () => {
+        try {
+          const res = await fetch(
+            `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${api_key}&language=en-US`
+          );
+          const data = await res.json();
+          const trailer = data.results.find(
+            result => result.type === 'Trailer' || result.type === 'Teaser'
+          );
+          console.log(trailer.key);
+          console.log(modal);
+          modal.innerHTML += `<iframe src="https://www.youtube.com/embed/${trailer.key}" allowfullscreen></iframe>`;
+        } catch (error) {
+          console.error('Error fetching trailer:', error);
+        }
+      });
     } catch (error) {
       console.error('Error displaying movie:', error);
     }
