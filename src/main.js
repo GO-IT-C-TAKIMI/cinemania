8411026ed53c23e8b95d7acdcd5bde45a6aeada0
gizemcoban <gizem.coban.r@gmail.com>
2024-10-14 00:33:50 +0300

    conflict duzenledim

--- src/main.js
+++ src/main.js
@@ -5,12 +5,6 @@ import { hero } from './js/hero';
 import { movielist } from './js/movielist';
 import { upcoming } from './js/upcoming';
 import { footer } from './js/footer';
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> mylibrary
 import theme from './js/theme';
 if (window.location.pathname === '/catalog.html') {
   header();
